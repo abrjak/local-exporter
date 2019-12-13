560dff7886a445b205cc6a0d05014b52e1e2d449
Abraham, Jakob <jakob.abraham@baloise.ch>
2019-12-13 16:39:28 +0100

    created expressions

--- model/appModel.js
+++ model/appModel.js
@@ -14,7 +14,7 @@ var resultset = [];
 Data.getData = function(result){
 
     const sql = `
-    SELECT * FROM test_data ORDER BY lp_id DESC LIMIT 3
+    SELECT * FROM test_data ORDER BY proc_end DESC LIMIT 4
     `;
 
     mysql.query(sql, function(err, rows){
@@ -23,21 +23,69 @@ Data.getData = function(result){
             result(null, err);
         } else {
 
-            sb.append('# HELP local_dur local duration.\n');
-            sb.append('# TYPE local_dur gauge\n');
+            // basic counter for proc dur
+            sb.append('# HELP local_dur_total total counter local duration.\n');
+            sb.append('# TYPE local_dur_total counter\n');
 
             rows.forEach(function(row){
 
-                sb.append('local_dur{scen="').append(row.scen_name).append('", pm="').append(row.pm_name).append('"} ').append(row.lp_dur).append('\n');
+                sb.append('local_dur_total{scen="').append(row.scen_name)
+                .append('", pm="').append(row.pm_name)
+                .append('", state="').append(row.proc_state)
+                .append('"} ').append(row.proc_dur)
+                .append(' ').append(new Date().getTime()).append('\n');
+
+            });
+            
+            // basic gauge for proc dur
+            sb.append('# HELP local_dur_seconds gauge local duration.\n');
+            sb.append('# TYPE local_dur_seconds gauge\n');
+
+            rows.forEach(function(row){
+
+                sb.append('local_dur_seconds{scen="').append(row.scen_name)
+                .append('", pm="').append(row.pm_name)
+                .append('", state="').append(row.proc_state)
+                .append('"} ').append(row.proc_dur).append('\n');
+
+            });
+            
+            // histogram bucket for proc dur
+            sb.append('# HELP local_dur_sec histogram local duration.\n');
+            sb.append('# TYPE local_dur_sec histogram\n');
+
+            rows.forEach(function(row){
+
+                sb.append('local_dur_sec_bucket{scen="').append(row.scen_name)
+                .append('", pm="').append(row.pm_name)
+                .append('", state="').append(row.proc_state)
+                .append('", le="100"} ').append(row.proc_dur).append('\n');
 
             });
 
-            sb.append('# HELP local_duration local duration.\n');
-            sb.append('# TYPE local_duration histogram\n');
+            // histogram sum for proc dur
+            sb.append('# HELP local_dur_sec histogram local duration.\n');
+            sb.append('# TYPE local_dur_sec histogram\n');
 
             rows.forEach(function(row){
 
-                sb.append('local_duration_bucket{scen="').append(row.scen_name).append('", pm="').append(row.pm_name).append('", le="100"} ').append(row.lp_dur).append('\n');
+                sb.append('local_dur_sec_sum{scen="').append(row.scen_name)
+                .append('", pm="').append(row.pm_name)
+                .append('", state="').append(row.proc_state)
+                .append('"} ').append(row.proc_dur).append('\n');
+
+            });
+
+            // histogram count for proc dur
+            sb.append('# HELP local_dur_sec histogram local duration.\n');
+            sb.append('# TYPE local_dur_sec histogram\n');
+
+            rows.forEach(function(row){
+
+                sb.append('local_dur_sec_count{scen="').append(row.scen_name)
+                .append('", pm="').append(row.pm_name)
+                .append('", state="').append(row.proc_state)
+                .append('"} ').append(row.proc_dur).append('\n');
 
             });
 
