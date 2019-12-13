d4ac300aee74bba0c6a887f47ad5f5aa4442b6b1
Abraham, Jakob <jakob.abraham@baloise.ch>
2019-12-13 16:39:06 +0100

    updated data objects in db

--- controller/appController.js
+++ controller/appController.js
@@ -3,10 +3,6 @@
 var Data = require('../model/appModel.js');
 var Entry = require('../model/entryModel.js');
 
-var x = 1;
-var y = 2;
-var z = 0;
-var a = 3;
 exports.get_data = function(req, res){
     Data.getData(function(err, content){
         res.send(content);
@@ -28,20 +24,35 @@ exports.post_data = function(req, res){
 setInterval(autoPoster, 15000);
 
 function autoPoster(){
+
+    // random entry 1
     var dur = Math.floor(Math.random() * 100) + 1;
-    var newEntry = JSON.parse('{"scen_name":"test", "pm_name":"pm_test", "count_succ":'+z+', "count_abort":4, "lp_id":'+x+', "lp_dur":'+dur+'}');
-    Entry.postData(newEntry, function(err, user){
-    });
+    var state = Math.floor(Math.random() * 3) + 1;
+
+    var newEntry = JSON.parse('{"scen_name":"test", "pm_name":"pm_test", "proc_state":' + state + ', "proc_dur":' + dur + '}');
+
+    Entry.postData(newEntry, function(err, user){});
+
+    // random entry 2
     dur = Math.floor(Math.random() * 100) + 1;
-    newEntry = JSON.parse('{"scen_name":"test", "pm_name":"pm_all", "count_succ":'+z+', "count_abort":4, "lp_id":'+y+', "lp_dur":'+dur+'}');
-    Entry.postData(newEntry, function(err, user){
-    });
+
+    newEntry = JSON.parse('{"scen_name":"test", "pm_name":"pm_main", "proc_state":' + state + ', "proc_dur":' + dur + '}');
+
+    Entry.postData(newEntry, function(err, user){});
+
+    // random entry 3
     dur = Math.floor(Math.random() * 100) + 1;
-    newEntry = JSON.parse('{"scen_name":"error", "pm_name":"pm_error", "count_succ":1, "count_abort":'+z+', "lp_id":'+a+', "lp_dur":'+dur+'}');
-    Entry.postData(newEntry, function(err, user){
-    });
-    x = x+3;
-    y = y+3;
-    a = a+3;
-    z++;
+    state = Math.floor(Math.random() * 3) + 1;
+
+    newEntry = JSON.parse('{"scen_name":"error_test", "pm_name":"pm_error", "proc_state":' + state + ', "proc_dur":' + dur + '}');
+
+    Entry.postData(newEntry, function(err, user){});
+
+    // perfect entry
+    //dur = Math.floor(Math.random() * 15) + 10;
+    dur = 10;
+    newEntry = JSON.parse('{"scen_name":"all_right", "pm_name":"pm_perfect", "proc_state":"1", "proc_dur":' + dur + '}');
+
+    Entry.postData(newEntry, function(err, user){});
+
 };
--- model/entryModel.js
+++ model/entryModel.js
@@ -3,13 +3,11 @@
 var mysql = require('../db.js');
 
 var Entry = function(entry){
-   this.id = entry.id;
+   this.proc_id = entry.proc_id;
    this.scen_name = entry.scen_name;
    this.pm_name = entry.pm_name;
-   this.count_succ = entry.count_succ;
-   this.count_abort = entry.count_abort;
-   this.lp_id = entry.lp_id;
-   this.lp_dur = entry.lp_dur;
+   this.proc_state = entry.proc_state;
+   this.proc_dur = entry.proc_dur;
 };
 
 Entry.postData = function(new_entry, result){
@@ -19,6 +17,16 @@ Entry.postData = function(new_entry, result){
             result(err, null);
         } else {
             result(null, res.insertId);
+
+            mysql.query("delete from test_data where proc_end < DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 30 SECOND)",function(err, res){
+                if(err){
+                    console.log("error: ", err);
+                    result(err, null);
+                } else {
+                    result(null, res.insertId);
+                }
+            });
+
         }
     });    
 };
