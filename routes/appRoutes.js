0c7b445e5841e824b70ceb88352cfb86b333e756
Abraham, Jakob <jakob.abraham@baloise.ch>
2019-12-19 16:20:05 +0100

    added new routes for errors

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -2,10 +2,20 @@
 
 module.exports = function(app){
     var ctrl = require('../controller/appController.js');
+    var ctrl_err = require('../controller/errorController.js');
     
     app.route('/metrics')
         .get(ctrl.get_data);
     
     app.route('/post')
         .post(ctrl.post_data);
+
+    app.route('/')
+        .get(ctrl_err.get_root);
+
+    app.route('/data')
+        .get(ctrl_err.get_error);
+
+    app.route('/search')
+        .post(ctrl_err.get_search);
 };
